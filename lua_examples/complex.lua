42c3b2611546b38046085237c55795b32c5e929a
Kevin Darlington <kevin.darlington@crowdstrike.com>
2025-01-30 09:14:14 -0600

    fix: trying to figure out how to get monitor add/delete in macos to work properly

--- lua_examples/complex.lua
+++ lua_examples/complex.lua
@@ -36,7 +36,12 @@ local hotplug_callback = function(status, vendor_id, product_id)
   -- If we are connected on macos, then set it up such that the external monitor is
   -- an extension of the macbook monitor.
   if status == "connected" then
-    os.execute('displayplacer "id:3145D954-9166-4630-537F-6A7A36E2B478 res:1792x1120 hz:59 color_depth:4 enabled:true scaling:on origin:(0,0) degree:0" "id:3FDCE03B-E3E4-24D1-82FE-C00ABF19A2B0 res:2560x1440 hz:59 color_depth:8 enabled:true scaling:off origin:(-2560,0) degree:0"')
+    print("Monitor connected")
+    os.execute('displayplacer "id:3FDCE03B-E3E4-24D1-82FE-C00ABF19A2B0 res:2560x1440 hz:59 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0" "id:3145D954-9166-4630-537F-6A7A36E2B478 res:1792x1120 hz:59 color_depth:4 enabled:true scaling:on origin:(2560,0) degree:0"')
+    -- os.execute('displayplacer "id:3145D954-9166-4630-537F-6A7A36E2B478 res:1792x1120 hz:59 color_depth:4 enabled:true scaling:on origin:(0,0) degree:0" "id:3FDCE03B-E3E4-24D1-82FE-C00ABF19A2B0 res:2560x1440 hz:59 color_depth:8 enabled:true scaling:off origin:(-2560,0) degree:0"')
+  else
+    print("Monitor disconnected")
+    os.execute('displayplacer "id:3FDCE03B-E3E4-24D1-82FE-C00ABF19A2B0 enabled:false"')
   end
 end
 
